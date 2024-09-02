718c5b5306badd65dc03dbf0450aa6b7c960e7c9
Joel Jino <joeljino04@gmail.com>
2024-09-02 18:44:40 +0530

    first commit

---
+++ Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-purple-950'>
+        <h1 className='font-extrabold text-white p-10 text-3xl text-center'>ToDo List Application</h1>
+    </div>
+  )
+}
+
+export default Header
